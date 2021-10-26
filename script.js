3f99fb20a58e72427f478f54e81ee1033db8d67c
Vidottizzz <vidottizzz@gmail.com>
2021-10-26 10:36:34 -0300

    adicionando botão flutuante para ir para o topo

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
+
+
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
